9b225b66c84dfba78eb2bb3f1c5cae95dc833370
Yucong Jin <yucongj@microsoft.com>
2019-11-21 15:01:04 +0800

    first demo

--- src/components/ReactDagEditor.tsx
+++ src/components/ReactDagEditor.tsx
@@ -1,9 +1,54 @@
 import * as React from "react";
 
-interface ReactDagEditorProps {
-
+interface ICanvasNode {
+  height: number,
+  width: number
+  x: number,
+  y: number,
+  label: string,
+  id: string;
 }
 
-export const ReactDagEditor: React.FunctionComponent<ReactDagEditorProps> = () => {
-  return <>ReactDagEditor</>;
+interface ICanvasEdge {
+  source: string;
+  target: string;
+}
+
+interface ICanvasData {
+  nodes: ICanvasNode[];
+  edges: ICanvasEdge[];
+}
+
+interface ReactDagEditorProps {
+  data: ICanvasData;
+}
+
+export const ReactDagEditor: React.FunctionComponent<ReactDagEditorProps> = (props) => {
+  return (
+    <svg width={10000} height={10000}>
+      {props.data.nodes.map(node => (
+        <g>
+          <rect width={node.width} height={node.height} x={node.x} y={node.y} style={{fill:"rgb(0,0,255)",strokeWidth:3, stroke: "rgb(0,0,0)"}} />
+          <text x={node.x} y={node.y + node.height / 2} fontSize={20}>{node.label}</text>
+        </g>
+      ))}
+      {props.data.edges.map(edge => {
+        const sourceNode = props.data.nodes.find(node => node.id === edge.source);
+
+        if (!sourceNode) {
+          throw "invalid source";
+        }
+
+        const targetNode = props.data.nodes.find(node => node.id === edge.target);
+
+        if (!targetNode) {
+          throw "invalid source";
+        }
+
+        return (
+          <line x1={sourceNode.x + sourceNode.width} y1={sourceNode.y + sourceNode.height} x2={targetNode.x} y2={targetNode.y} style={{stroke: "rgb(255,0,0)", strokeWidth:2 }} />
+        )
+      })}
+    </svg>
+  );
 }
